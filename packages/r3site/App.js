ff4af1d9397c53d133101d57217a8b7405a259dd
Md. Hussainul Islam Sajib <m.hussainul.islam@gmail.com>
2021-03-16 19:12:38 -0400

    react and react dom added; running

---
+++ packages/r3site/App.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function App() {
+    return (
+        <div>
+            <section>
+                <h1>Hussain</h1>
+                <h1>Header 1</h1>
+                <h2>Header 2</h2>
+                <h3>Header 3</h3>
+                <h4>Header 4</h4>
+                <h5>Header 5</h5>
+                <h6>Header 6</h6>
+            </section>
+            <section>
+                <h1>Cards</h1>
+                <div class="card">Hello World</div>
+            </section>
+        </div>
+    )
+}
+
+export default App
