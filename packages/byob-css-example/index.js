e706e5641baac08c8d979607cea1075af36a726f
Chris Johns <chrisfotosmusic@gmail.com>
2021-03-17 23:21:15 -0400

    fix file references in html files

--- packages/byob-css-example/index.js
+++ packages/byob-css-example/index.js
@@ -1,11 +1,11 @@
 import "byob-css";
 
-// let navs = Array.from(document.getElementsByTagName('nav'))
-// navs.forEach(n => {
-//     let children = Array.from(n.childNodes)
-//     children.forEach(c => {
-//         if(c.nodeName === 'A'){
-//             c.tabIndex = '0'
-//         }
-//     })
-// })
+let navs = Array.from(document.getElementsByTagName('nav'))
+navs.forEach(n => {
+    let children = Array.from(n.childNodes)
+    children.forEach(c => {
+        if(c.nodeName === 'A'){
+            c.tabIndex = '0'
+        }
+    })
+})
